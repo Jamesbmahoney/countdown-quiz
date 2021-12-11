89f638aea60814711329a86b02c074477d9667f0
James Barry <jbm03003@gmail.com>
2021-12-11 13:03:43 -0500

    added a new js file for questions

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "Which Final Fantasy game is an MMORPG?",
+        choice: ["FFIII", "FFVII", "FFXI", "FFXV"], 
+        answer: "FFXI"
+    },
+    {
+        question: "Which of these games came first?",
+        choice: ["Xenosaga", "Xenogears", "Xenoblade Chronicles", "Xenoblad Chronicles 3D"],
+        answer: "Xenogears"
+    },
+    {
+        question: "Who is Zelda in The Legend of Zelda?",
+        choice: ["Princess", "Hero", "King", "Villian"],
+        answer: "Princess"
+    },
+    {
+        question: "What is the name of Mario's brother?",
+        choice: ["Eugene", "Harry", "Player 2", "Luigi"],
+        answer: "Luigi"
+    },
+    {
+        question: "Kingdom Hearts was a collaboration between Square-Enix and which company?",
+        choice: ["Disney", "Warner Brothers", "Nintendo", "Paramount"],
+        answer: "Disney"
+    }
+]
