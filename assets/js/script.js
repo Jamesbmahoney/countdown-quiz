31c4ab53d04167177146e0c8c3af985cd2fecd7f
James Barry <jbm03003@gmail.com>
2021-12-13 15:40:35 -0500

    added functioning timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,25 @@ var quizEl = document.querySelector("#quiz-questions");
 var questsEl = document.querySelector("#quests");
 var answersEl = document.querySelector("#answers");
 
+var countdownTimer;
+var time =  60;
 
 function quizStart() {
     openingEl.setAttribute("class", "hide");
 
     quizEl.setAttribute("class", "show");
-};
 
-function toQuestions() {
-    var quizQuestions = questions[currentQuestionIndex]
+    countdownTimer = setInterval(countdown, 1000);
 
-    var qTitleEl = document.getElementById("question-section");
-    qTitleEl.textContent =currentQuestion.title;
-
-    answersEl.innerHTML = "";
+    timerEl.textContent = time;
 
 };
+
+function countdown() {
+    time--;
+    timerEl.textContent = time;
+};
+
 
 startEl.addEventListener("click", quizStart);
 
