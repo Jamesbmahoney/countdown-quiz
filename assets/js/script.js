a802cbfc11f0e50776d4e3ccdc88c057b56ace9a
James Barry <jbm03003@gmail.com>
2021-12-14 15:59:01 -0500

    added functionality to questions, to move forward if corrent, penalize if wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@ var timerEl = document.querySelector("#count-clock");
 var startEl = document.querySelector("#start-btn");
 var openingEl = document.querySelector("#start-page");
 var quizEl = document.querySelector("#quiz-questions");
-var questsEl = document.querySelector("#quests");
-var answersEl = document.querySelector("#answers");
+var answersEL = document.querySelector("#answer-section");
+var feedbackEL = document.querySelector("#feedback");
 
 var countdownTimer;
 var time =  60;
+var currentQuestionIndex = 0;
 
 function quizStart() {
     openingEl.setAttribute("class", "hide");
@@ -17,12 +18,70 @@ function quizStart() {
 
     timerEl.textContent = time;
 
+    callQuestions();
+
 };
 
 function countdown() {
     time--;
     timerEl.textContent = time;
 };
+
+function callQuestions() {
+    var quizQuestions = questions[currentQuestionIndex];
+
+    var actualQuestion = document.getElementById("question-section");
+    actualQuestion.textContent = quizQuestions.question;    
+
+answersEL.innerHTML= "";    
+
+quizQuestions.choice.forEach(function(choice, i){
+    var choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute("class", "choice");
+    choiceBtn.setAttribute("value", choice);
+
+    choiceBtn.textContent = i + 1 + ". " + choice;
+    choiceBtn.onclick = questionClick;
+
+    answersEL.appendChild(choiceBtn);
+});
+
+    //var currentChoicesEl = document.getElementById("answer-section");
+    //currentChoicesEl.textContent = quizQuestions.choice;       
+}
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+
+        timerEl.textContent = time;
+        feedbackEL.textContent = "Wrong!";      
+    } else {
+        feedbackEL.textContent = "Correct!";
+    }
+
+    feedbackEL.setAttribute("class", "feedback");
+    setTimeout(function() {
+        feedbackEL.setAttribute("class", "feedback hide");
+    }, 1000)
+
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+        quizEnd();
+    } else {
+        callQuestions();
+    }
+};
+
+
+
+
+  
 
 
 startEl.addEventListener("click", quizStart);
